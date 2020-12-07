8c05366c34d6db4b84d2042a3be4638dd04a3aef
Mike Centers <michael.centers@icloud.com>
2020-12-07 11:18:19 -0500

    Initial Commit

---
+++ Shared/SwiftUI_Sidebar_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sidebar_TemplateApp.swift
+//  Shared
+//
+//  Created by Mike Centers on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sidebar_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
