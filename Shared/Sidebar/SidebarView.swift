c0a3c981a3ba57f24b4cdf79cf231f8e1e203daf
Mike Centers <michael.centers@icloud.com>
2020-12-08 10:08:14 -0500

    Styled the footer buttons.
    Will need to implement better alignment.

--- Shared/Sidebar/SidebarView.swift
+++ Shared/Sidebar/SidebarView.swift
@@ -8,12 +8,65 @@
 import SwiftUI
 
 struct SidebarView: View {
+    var isTapped: Bool = false
     
     var body: some View {
-        ScrollView {
-            Text("Sidebar View")
+        VStack {
+            ScrollView {
+                Text("Header Section")
+                
+                Text("Some body")
+                
+                
+            }
+            .background(Color.green)
             
-        }
+            
+            
+            /*
+             Footer Section
+             */
+            
+            //MARK: FixMe:
+            // - Icon should be center aligned.
+            // - Text should be leading aligned.
+            VStack(spacing: 15) {
+                Divider()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                    NavigationLink(
+                        destination: Text("About View"),
+                        label: {
+                            Label("About", systemImage: "info.circle")
+                                .font(Font.title3.weight(.bold))
+                        }
+                    )
+                    
+                    
+                    NavigationLink(
+                        destination: Text("Contact View"),
+                        label: {
+                            Label("Contact", systemImage: "envelope")
+                                .font(Font.title3.weight(.bold))
+                        }
+                    )
+                    
+                    
+                    NavigationLink(
+                        destination: Text("Destination 3"),
+                        label: {
+                            Label("Link 3", systemImage: "photo")
+                                .font(Font.title3.weight(.bold))
+                        }
+                    )
+                }
+                .foregroundColor(.secondary)
+            }
+            .padding(.bottom, 20)
+            
+            
+        }.navigationBarTitle("Sidebar Title")
     }
 }
 
