ac50e25367f700a1a186dbb86b76498e3000bc13
Mike Centers <michael.centers@icloud.com>
2020-12-10 11:14:37 -0500

    Established the Contact Us View Template.
    Established error view for email not setup.

--- Shared/System Views/Contact Us/ContactUsView.swift
+++ Shared/System Views/Contact Us/ContactUsView.swift
@@ -6,11 +6,70 @@
 //
 
 import SwiftUI
+import MessageUI
 
 struct ContactUsView: View {
     
+    @State private var emailRecipients: [String] = []
+    @State private var emailSubject: String = ""
+    
+    
+    @State private var result: Result<MFMailComposeResult, Error>? = nil
+    @State var isShowingMailView: Bool = false
+    private var canSendEmails: Bool {
+        return MFMailComposeViewController.canSendMail()
+    }
+    
     var body: some View {
-        Text("Contact Us View")
+        ScrollView {
+            Image(systemName: "person.fill.questionmark")
+                .font(.system(size: 125, weight: .medium))
+                .foregroundColor(.blue)
+                .padding(.leading, 35)  // Try to offset image
+            
+            VStack(spacing: 20) {
+                AppScrollSectionView(titleText: "Need Help?",
+                                     bodyText: "Contact support for any issues regarding the use of this app.",
+                                     buttonText: "Contact Support",
+                                     action: {
+                                        self.emailRecipients = ["example@email.com"]
+                                        self.emailSubject = "Support: "
+                                        self.isShowingMailView.toggle()
+                                     }
+                )
+                
+                AppScrollSectionView(titleText: "Feature Request",
+                                     bodyText: "Help drive the direction of this app by submitting a feature request.",
+                                     buttonText: "Request Feature",
+                                     action: {
+                                        self.emailRecipients = ["example@email.com"]
+                                        self.emailSubject = "Feature Request: "
+                                        self.isShowingMailView.toggle()
+                                     }
+                )
+                
+                AppScrollSectionView(titleText: "Feedback",
+                                     bodyText: "While this app is tested heavily prior to release, errors within any app are possible. Issue feedback for app errors you may have found.",
+                                     buttonText: "Issue Feedback",
+                                     action: {
+                                        self.emailRecipients = ["example@email.com"]
+                                        self.emailSubject = "Feedback: "
+                                        self.isShowingMailView.toggle()
+                                     }
+                )
+            }
+            
+        }
+        .navigationTitle("Contact Us")
+        .sheet(isPresented: $isShowingMailView) {
+            if self.canSendEmails {
+                MailView(result: self.$result,
+                         recipients: self.emailRecipients,
+                         subject: self.emailSubject)
+            } else {
+                EmailRequiredView()
+            }
+        }
     }
 }
 
